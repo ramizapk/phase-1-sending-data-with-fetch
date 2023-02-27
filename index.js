baf1b1c742d7ea7af33b1bcefd97e6ccbe55d404
Ramiz Ali <ramiz.apk7@gmail.com>
2023-02-27 05:50:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email) {
+    return (fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        }),
+    }).then((response) => response.json()).then((object) => {
+        console.log(object.id);
+    }).catch(error => {
+        document.body.innerHTML = error;
+        // Handle the error here
+    }));
+
+
+}
+
+
+let form = document.getElementById("form");
+function handleForm(event) {
+    event.preventDefault();
+    console.log(form.name.value);
+    submitData(form.name.value, form.email.value);
+}
+form.addEventListener('submit', handleForm);
